6abd5cbd6102e1da545d7a3c0f95b22f042df2e9
Kevin Hester <kevinh@geeksville.com>
2013-09-25 11:28:36 -1000

    our build script

---
+++ build-xeralux.sh
@@ -0,0 +1,14 @@
+   $ git clone https://github.com/json-c/json-c.git
+    $ cd json-c
+    $ sh autogen.sh
+
+Then 
+
+    $ ./configure
+    $ make
+    $ make install
+
+To build and run the test programs run 
+
+    $ make check
+
